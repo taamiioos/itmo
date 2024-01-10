e3b4f8fd32ea5d3e3292d815f9423431851320c6
Tata Mitina <tatamitina19@gmail.com>
2024-01-10 18:51:55 +0300

    w

---
+++ web/web-lab1/js/buttonHandler.js
@@ -0,0 +1,48 @@
+const tableBody = document.querySelector('#table tbody');
+document.addEventListener("DOMContentLoaded", function () {
+    const sendButton = document.getElementById('sendButton');
+    const resetButton = document.getElementById('resetButton');
+
+    sendButton.addEventListener('click', send);
+    resetButton.addEventListener('click', reset);
+});
+
+function send() {
+    if (validateXYR()) {
+        console.log("SENDING");
+        $.ajax({
+            type: "POST",
+            url: "../php/base.php",
+            dataType: "json",
+            data: {
+                x: x,
+                y: y.value,
+                r: r.value,
+                time: new Date().getTimezoneOffset()
+            },
+            success: function (data) {
+                addInTable(data);
+            },
+            error: function (XHR, status, error) {
+                console.error("Error:", error);
+            }
+        });
+    } else {
+        console.error("Validation failed");
+    }
+}
+
+function reset() {
+    tableBody.innerHTML = '';
+    $.ajax({
+        type: "POST",
+        url: "../php/reset.php",
+        success: function () {
+            console.log("Data cleared.");
+        },
+        error: function (XHR, status, error) {
+            console.error("Error:", error);
+        }
+    });
+}
+
---
+++ web/web-lab1/js/table.js
@@ -0,0 +1,31 @@
+function loadHistory() {
+    $.ajax({
+        type: "GET",
+        url: "../php/base.php",
+        dataType: "json",
+        success: function (data) {
+            addInTable(data);
+        },
+        error: function (XHR, status, error) {
+            console.error("Error:", error);
+        }
+    });
+}
+
+function addInTable(data) {
+    tableBody.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        const row = document.createElement('tr');
+        row.innerHTML = "<td>" + data[i]['x'] + "</td>" +
+            "<td>" + data[i]['y'] + "</td>" +
+            "<td>" + data[i]['r'] + "</td>" +
+            "<td>" + data[i]['flag'] + "</td>" +
+            "<td>" + data[i]['start_time'] + "</td>" +
+            "<td>" + data[i]['time'] + "</td>";
+        tableBody.appendChild(row);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadHistory();
+});
