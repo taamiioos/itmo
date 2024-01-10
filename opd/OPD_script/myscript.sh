e0224fea04e330083bf67c1fc5d7876d3abfda0f
Tata Mitina <113043652+taamiioos@users.noreply.github.com>
2024-01-10 21:04:33 +0300

    opd 1-st sem

---
+++ opd/OPD_script/myscript.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+mkdir lab0
+cd lab0
+mkdir cubone3
+cd cubone3
+mkdir togekiss
+touch drapion
+mkdir jolteon
+cd ..
+touch porygon6
+touch scizor8
+mkdir vulpix5
+cd vulpix5
+mkdir growlithe
+touch gyarados
+mkdir frillish
+mkdir slowking
+cd ..
+touch wigglytuff2
+mkdir wingull6
+cd wingull6
+touch nincada
+mkdir nidoking
+touch umbreon
+mkdir blitzle
+mkdir chimecho
+cd ..
+cd cubone3
+cat > drapion <<-EOF
+weight=135.6 height=51.0 atk=9 def=11
+EOF
+cd ..
+cat > porygon6 <<-EOF
+Живет
+Urban
+EOF
+cat > scizor8 <<-EOF
+Развитые способности Light Metal
+Steadfast
+EOF
+cd vulpix5
+chmod 777 gyarados
+cat > gyarados <<-EOF
+Тип диеты Carnivore
+EOF
+cd ..
+cat > wigglytuff2 <<-EOF
+Способности This
+is dummy text when pokemon does not contain something. It is better
+than NPE!
+EOF
+cd wingull6
+cat > nincada <<-EOF
+satk=3 sdef=3 spd=4
+EOF
+chmod 777 umbreon
+cat > umbreon <<-EOF
+Развитые способности
+Inner Focus
+EOF
+cd
+cd lab0
+chmod 337 cubone3
+cd cubone3
+chmod 537 togekiss
+chmod 404 drapion
+chmod 752 jolteon
+cd ..
+chmod 604 porygon6
+chmod 046 scizor8
+chmod 375 vulpix5
+cd vulpix5
+chmod 311 growlithe
+chmod 444 gyarados
+chmod 755 frillish
+chmod 777 slowking	
+cd ..
+chmod 060 wigglytuff2
+chmod 555 wingull6
+cd wingull6
+chmod 600 nincada
+chmod 736 nidoking	
+chmod 400 umbreon
+chmod 373 blitzle
+chmod 357 chimecho
+cd 
+cd lab0
+chmod 777 wigglytuff2
+chmod 777 wingull6
+chmod 777 cubone3
+chmod 777 vulpix5
+cd cubone3
+chmod 777 togekiss
+cd ..
+cd wingull6 
+chmod 777 nincada
+cd ..
+ln wigglytuff2 wingull6/umbreonwigglytuff
+cp porygon6 cubone3/drapionporygon
+ln -s cubone3 Copy_49
+cp wigglytuff2 vulpix5/slowking
+rm wingull6/umbreonwigglytuff
+cp wigglytuff2 wingull6/umbreonwigglytuff
+cat wingull6/nincada wingull6/nincada > scizor8_34
+cp -R cubone3 cubone3/togekiss
+cd
+cd lab0
+mkdir tmp
+cd tmp
+touch file
+cd ..
+chmod 777 vulpix5/growlithe
+chmod 777 wingull6/blitzle
+chmod 777 wingull6/chimecho
+(ls -R | grep "s$" | wc) > /tmp/file
+(ls -tr |grep on | head -n4) 2>/dev/null
+(ls -R |grep "g$" | sort) 2>&1
+ls -R ./vulpix5/* | grep on
+(ls -R | grep "^s" | sort -nr | wc -l) 2>/dev/null
+(cat ./cubone3/drapion ./vulpix5/gyarados ./wingull6/nincada | sort) 2>&1
+chmod 777 vulpix5/gyarados
+rm porygon6
+rm vulpix5/gyarados
+rm Copy_49
+rm wingull6/umbreonwigglytuff
+cd cubone3
+rm -rf *
+cd ..
+rmdir cubone3
+
+
+
+
+
+
+
